2df61c582101b3c6467cae3cf5d2a060768ed07c
mdtaju <tajuttt512ctg@gmail.com>
2023-12-18 22:16:19 +0600

    public form data

--- src/Shared/PublicForm/PublicForm.js
+++ src/Shared/PublicForm/PublicForm.js
@@ -1,20 +1,24 @@
-import { Container } from '@mui/material';
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
+import { Container } from "@mui/material";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useGetSingleFormQuery } from "../../features/student/studentApi";
 
 const PublicForm = () => {
-    const params = useParams();
-    console.log(params.id)
-    return (
-        <div className='py-40'>
-            <div className='mx-auto '>
-               <Container className='bg-red-500' fixed>
-               <h1 className='text-4xl font-bold '> Hello public !!!!{params?.id}</h1>
-               </Container>
-            </div>
-        </div>
-    );
+  const params = useParams();
+  const { data } = useGetSingleFormQuery(params.id);
+  console.log(data);
+  return (
+    <div className="py-40">
+      <div className="mx-auto ">
+        <Container className="bg-red-500" fixed>
+          <h1 className="text-4xl font-bold ">
+            {" "}
+            Hello public !!!!{params?.id}
+          </h1>
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export default PublicForm;
