e755b5d437ee804bd25ea11dcbe64aa71b6ef0f9
Niloy-Dey <niloydeyce@gmail.com>
2024-01-01 23:30:43 +0600

    solved

--- src/Components/StudentDashboard/StudentProfile/TestScores/TestScores.js
+++ src/Components/StudentDashboard/StudentProfile/TestScores/TestScores.js
@@ -28,14 +28,7 @@ const TestScores = ({
   otherDetails,
   setOtherDetails,
 
-  specialExamsType,
-  setSpecialExamsType,
-  specialExamOtherDetails,
-  setSpecialExamOtherDetails,
-  selectSpecialExamType,
-  setSelectSpecialExamType,
-  specialExamScore,
-  setSpecialExamScore,
+ 
 }) => {
   const examType = [
     "",
@@ -50,22 +43,8 @@ const TestScores = ({
     "Others",
   ];
 
-  const specialExamType = ["", "Yes", "No", "Others"];
-
-  const specialExamOptions = [
-    "",
-    "Graduate Record Examination (GRE)",
-    "Graduate Management Admission Test (GMAT)",
-    "Scholastic Assessment Test (SAT)",
-    "American College testing (ACT)",
-    "Medical College Admission Test (MCAT)",
-    "Law School Admission Test (LSAT)",
-    "Dental Admission Test(DAT)",
-    "Graduate Aptitude Test in Engineering (GATE)",
-    "Common Admission (CAT)",
-  ];
+ 
   return (
-    <Container>
       <div className="shadow-md w-full p-4 sm:p-6 bg-white rounded-lg mt-10">
         <h1 className="text-2xl font-bold text-gray-600">
           English Proficiency Details
@@ -196,62 +175,7 @@ const TestScores = ({
         </div>
       </div>
 
-      {/* special exam section  */}
-
-      <div className="shadow-md w-full p-4 sm:p-6 bg-white rounded-lg mt-10">
-        <h1 className="text-2xl font-bold text-gray-600">Special Test Score</h1>
-        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 gap-6 my-6">
-          <SelectInput
-            title={"Special Exam Type"}
-            placeholder="Select exam type"
-            isRequired
-            selectState={specialExamsType}
-            setSelectState={setSpecialExamsType}
-            optionsArray={specialExamType}
-          />
-        </div>
-
-        <div className="">
-          {/* Others exam functionalities */}
-          {specialExamsType === "Others" && (
-            <TextArea
-              title={"Details"}
-              placeholder="Enter Full Details"
-              type="text"
-              value={specialExamOtherDetails}
-              onChange={(e) => setSpecialExamOtherDetails(e.target.value)}
-            />
-          )}
-
-          <div className="grid  grid-cols-2  gap-6 my-6">
-            {specialExamsType === "Yes" && (
-              <>
-                <SelectInput
-                  title={"Special Exam "}
-                  placeholder="Select exam "
-                  isRequired
-                  selectState={selectSpecialExamType}
-                  setSelectState={setSelectSpecialExamType}
-                  optionsArray={specialExamOptions}
-                />
-
-                <Input
-                  title={"Test Score"}
-                  isRequired
-                  required
-                  placeholder="Overall Score"
-                  type="number"
-                  value={specialExamScore}
-                  onChange={(e) => setSpecialExamScore(e.target.value)}
-                  max={10}
-                  min={0}
-                />
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-    </Container>
+    
   );
 };
 
